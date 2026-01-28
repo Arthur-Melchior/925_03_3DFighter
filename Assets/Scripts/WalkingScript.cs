83600664c56a490993db64826371dc449578d916
Arthur-Melchior <108128616+Arthur-Melchior@users.noreply.github.com>
2026-01-28 18:25:18 +0100

    step commit

--- Assets/Scripts/WalkingScript.cs
+++ Assets/Scripts/WalkingScript.cs
@@ -8,16 +8,17 @@ using UnityEngine.InputSystem;
 public class WalkingScript : MonoBehaviour
 {
     private static readonly int Velocity = Animator.StringToHash("Velocity");
-    private static readonly int RotationDifference = Animator.StringToHash("RotationDifference");
     private static readonly int Strife = Animator.StringToHash("Strife");
+    private static readonly int Grounded = Animator.StringToHash("Grounded");
 
     public float speed = 20f;
-    public float turnSpeed = 10f;
+    public float jumpHeight = 2f;
 
     private Animator _animator;
     private CharacterController _cc;
     private Vector3 _velocity;
     private Camera _camera;
+    private bool _hasJumped;
 
     private void Start()
     {
@@ -28,23 +29,26 @@ public class WalkingScript : MonoBehaviour
 
     private void Update()
     {
-        //_velocity.z *= transform.forward.z;
-        //to keep running ahead
         _cc.SimpleMove(new Vector3(
             _velocity.z * _camera.transform.forward.x + _velocity.x * _camera.transform.right.x,
             0,
             _velocity.z * _camera.transform.forward.z + _velocity.x * _camera.transform.right.z));
 
+        if (_hasJumped)
+        {
+            _cc.Move(new Vector3(0, _velocity.y * Time.deltaTime, 0));
+        }
+
         //to rotate to camera when moving
-        // var cameraRotationDifference = Mathf.DeltaAngle(_camera.transform.eulerAngles.y, transform.eulerAngles.y);
         if (_velocity.sqrMagnitude > 0f)
         {
-            //transform.rotation = new Quaternion(0, _camera.transform.rotation.y, 0, 0);
-            var test = new Quaternion(transform.rotation.x, _camera.transform.rotation.y, transform.rotation.z,transform.rotation.w);
-            transform.rotation = test;
-            // var rotationDirection = cameraRotationDifference > 0 ? -1 : 1;
-            // transform.Rotate(new Vector3(0, rotationDirection * turnSpeed * Time.deltaTime, 0));
-            // _animator.SetFloat(RotationDifference, cameraRotationDifference);
+            transform.eulerAngles = new Vector3(0, _camera.transform.eulerAngles.y, 0);
+        }
+
+        if (_cc.isGrounded && _hasJumped)
+        {
+            _hasJumped = false;
+            _animator.SetBool(Grounded, true);
         }
     }
 
@@ -53,24 +57,16 @@ public class WalkingScript : MonoBehaviour
         var value = ctx.ReadValue<Vector2>();
         _velocity = new Vector3(value.x, 0, value.y) * speed;
 
-
         //animations
         _animator.SetFloat(Velocity, value.y);
         _animator.SetFloat(Strife, value.x);
-        //_animator.SetFloat(RotationDifference, rotationDifference);
-    }
-
-    public void OnLook(InputAction.CallbackContext ctx)
-    {
     }
 
     public void OnJump(InputAction.CallbackContext ctx)
     {
-        _animator.SetTrigger("Jump");
-    }
-
-    public void ChangeRotation(float value)
-    {
-        transform.Rotate(new Vector3(0, value, 0));
+        if (!_cc.isGrounded || _hasJumped) return;
+        _velocity.y = Mathf.Sqrt(Mathf.Abs(jumpHeight * 2f * Physics.gravity.y));
+        _hasJumped = true;
+        _animator.SetBool(Grounded, false);
     }
 }
