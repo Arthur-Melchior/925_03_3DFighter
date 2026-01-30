62df4708339e55e4469fe851068f21f63c41e3f1
Arthur-Melchior <108128616+Arthur-Melchior@users.noreply.github.com>
2026-01-30 10:07:04 +0100

    step commit

--- Assets/Scripts/WalkingScript.cs
+++ Assets/Scripts/WalkingScript.cs
@@ -1,4 +1,7 @@
 using System;
+using System.Collections;
+using System.Linq;
+using Unity.Cinemachine;
 using UnityEditor.Timeline.Actions;
 using UnityEngine;
 using UnityEngine.InputSystem;
@@ -11,14 +14,24 @@ public class WalkingScript : MonoBehaviour
     private static readonly int Strife = Animator.StringToHash("Strife");
     private static readonly int Grounded = Animator.StringToHash("Grounded");
     private static readonly int Jumped = Animator.StringToHash("Jumped");
+    private static readonly int InCombatAnimation = Animator.StringToHash("InCombat");
 
     public float speed = 20f;
     public float jumpHeight = 2f;
+    public float coyoteTime = 0.2f;
+    public bool inCombat;
+    public CinemachineCamera cinemachineCamera;
 
     private Animator _animator;
     private CharacterController _cc;
     private Vector3 _velocity;
     private Camera _camera;
+    private bool _grounded = true;
+    private bool _jumped;
+    private bool _moveCanceled;
+
+    private Collider[] _hits;
+    private float _aggroAreaRadius;
 
     private void Start()
     {
@@ -29,30 +42,96 @@ public class WalkingScript : MonoBehaviour
 
     private void Update()
     {
-        _velocity.y += Physics.gravity.y * Time.deltaTime;
+        if (!_cc.isGrounded)
+            _velocity.y += Physics.gravity.y * Time.deltaTime;
+
         var move = new Vector3(
             _velocity.z * _camera.transform.forward.x + _velocity.x * _camera.transform.right.x,
             _velocity.y,
             _velocity.z * _camera.transform.forward.z + _velocity.x * _camera.transform.right.z) * Time.deltaTime;
         _cc.Move(move);
 
-        transform.forward = new Vector3(move.x, 0, move.z);
-        _animator.SetBool(Grounded, _cc.isGrounded);
+        //for coyote time
+        //checks if in the air and hasn't jumped
+        if (_grounded && !_cc.isGrounded && !_jumped)
+        {
+            StartCoroutine(StartCoyoteTime(coyoteTime));
+        }
+        else if (!_grounded && _cc.isGrounded)
+        {
+            StopAllCoroutines();
+            _grounded = true;
+            _jumped = false;
+            _velocity.y = 0;
+        }
+
+        if (!_moveCanceled && !inCombat)
+            transform.forward = new Vector3(move.x, 0, move.z);
+        else if (inCombat)
+            cinemachineCamera.Target.LookAtTarget = FindClosestEnemy().transform;
+
+
+        _animator.SetBool(Grounded, _grounded);
     }
 
     public void OnMove(InputAction.CallbackContext ctx)
     {
-        var value = ctx.ReadValue<Vector2>();
-        _velocity = new Vector3(value.x, 0, value.y) * speed;
+        _moveCanceled = ctx.canceled;
+
+        var value = ctx.ReadValue<Vector2>() * speed;
+        _velocity.x = value.x;
+        _velocity.z = value.y;
 
         //animations
-        _animator.SetFloat(Velocity, Mathf.Clamp(Mathf.Abs(value.y + value.x), 0, 1));
+        if (inCombat)
+        {
+            _animator.SetFloat(Velocity, value.y);
+            _animator.SetFloat(Strife, value.x);
+        }
+        else
+        {
+            _animator.SetFloat(Velocity, Mathf.Clamp(Math.Abs(value.y) + Math.Abs(value.x), 0, 1));
+        }
     }
 
     public void OnJump(InputAction.CallbackContext ctx)
     {
-        if (!_cc.isGrounded) return;
+        if (_jumped || !_grounded) return;
         _velocity.y = Mathf.Sqrt(2f * jumpHeight * -Physics.gravity.y);
+        _jumped = true;
+        _grounded = false;
         _animator.SetTrigger(Jumped);
+    }
+
+    public void StartCombat(InputAction.CallbackContext ctx)
+    {
+        inCombat = !inCombat;
+        _animator.SetBool(InCombatAnimation, inCombat);
+    }
+
+    private Collider FindClosestEnemy()
+    {
+        Physics.OverlapSphereNonAlloc(transform.position, _aggroAreaRadius, _hits);
+        var minDist = 100f;
+        Collider closestEnemy = null;
+
+        foreach (var col in _hits)
+        {
+            if (!col.CompareTag("Enemy")) continue;
+
+            if (Vector3.Distance(transform.position, col.transform.position) < minDist)
+            {
+                closestEnemy = col;
+            }
+        }
+
+        return closestEnemy;
+    }
+
+    private IEnumerator StartCoyoteTime(float time)
+    {
+        yield return new WaitForSeconds(time);
+        _grounded = false;
+        _jumped = true;
     }
 }
